a6f4c65cdd14555553e1a3d3d73002c0086026c0
eliasjrdev <eliasjrcosta886@gmail.com>
2025-12-06 12:00:50 -0300

    Adcionando links da emergent

--- src/components/BioLinks.jsx
+++ src/components/BioLinks.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useRef, useState } from 'react';
-import { FaBriefcase, FaTags, FaGraduationCap, FaTshirt, FaDesktop, FaLinkedin, FaGithub, FaRocket, FaLanguage, FaInternetExplorer } from 'react-icons/fa';
+import { FaBriefcase, FaTags, FaGraduationCap, FaTshirt, FaDesktop, FaLinkedin, FaGithub, FaRocket, FaLanguage, FaInternetExplorer, FaRobot, FaTextHeight, FaFile } from 'react-icons/fa';
 import Animacao from './Animacao';
 const links = [
+  { icon: <FaRobot />, label: 'Link da Emergent', url: 'https://app.emergent.sh/?via=costa' },
+  { icon: <FaFile />, label: 'Link do prompt', url: 'https://drive.google.com/file/d/1WvAcXmZvgToWXJbYsz8DWQKBz8xfVseY/view?usp=sharing' },
   { icon: <FaRocket />, label: 'Desconto na Rocketseat |', extra: 'Cupom: ELIASJR', url: 'https://www.rocketseat.com.br/oferta/influencer/v2/eliasjr' },
   { icon: <FaTshirt />, label: 'Melhores camisetas de t.i |', extra: 'Cupom: ELIASJR10', url: 'https://www.useti.shop/useti?utm_source=instagram&utm_medium=afiliado&utm_campaign=eliasjrdev' },
   { icon: <FaLanguage />, label: 'Melhor escola de inglês para Techs', url: 'https://api.whatsapp.com/send?phone=5583993611976' },
